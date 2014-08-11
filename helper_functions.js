d00bf6b6536368b60f38a35cb5eb2c90dbe26f73
Juan <juan.argote@gmail.com>
2014-08-11 15:57:23 -0700

    Refactored file structure.

---
+++ helper_functions.js
@@ -0,0 +1,27 @@
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+
+var format = d3.time.format("%Y-%m-%d %H:%M:%S %Z")
+
+function color_fill(d){
+  if (d.value['sensor'] == 'network'){
+    return "coral";
+  } else if (d.value['sensor'] == 'network') {
+    return "green";
+  } else {
+    return "steelblue";
+  }
+}
+
+function color_fill2(d){
+  if (d['sensor'] == 'network'){
+    return "coral";
+  } else if (d['sensor'] == 'network') {
+    return "green";
+  } else {
+    return "steelblue";
+  }
+}
